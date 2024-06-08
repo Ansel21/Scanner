f7cdae5bfa11f78527f445d09caabfc4bff1014e
Ansel21 <anselevander39@gmail.com>
2024-06-08 17:43:14 +0530

    Inital Setup

---
+++ app/(tabs)/Navigator/AppNavigator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {View} from 'react-native'
+import HomePage from "@/app/Screens/Homepage";
+
+     const Pages = 
+    [
+       { name:"HomePage", component:HomePage},
+    
+    ];
+
+    export default Pages;
---
+++ app/Screens/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {View,Text,StyleSheet} from 'react-native';
+const HomePage = () => {
+
+    return(
+        <View>
+            <Text>From Homepage</Text>
+        </View>
+    )
+}
+export default HomePage;
